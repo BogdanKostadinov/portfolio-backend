066ca496a7fcb89f2f020a5975512c94390d3c75
Bogdan Kostadinov <bk7907k@gre.ac.uk>
2021-11-16 16:45:27 +0000

    health check and secrets

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -34,22 +38,32 @@ namespace portfolio_backend
         {
             BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
             BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+            var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
 
             services.AddSingleton<IMongoClient>(ServiceProvider =>
             {
-                var settings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-                return new MongoClient(settings.ConnectionString);
+
+                return new MongoClient(mongoDbSettings.ConnectionString);
             });
 
             services.AddSingleton<IFeedbackRepository, MongoDbFeedbackRepository>();
 
-            services.AddControllers(options => {
+            services.AddControllers(options =>
+            {
                 options.SuppressAsyncSuffixInActionNames = false;
             });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "portfolio_backend", Version = "v1" });
             });
+
+            services.AddHealthChecks()
+            .AddMongoDb(
+                mongoDbSettings.ConnectionString,
+                name: "mongodb",
+                timeout: TimeSpan.FromSeconds(3),
+                tags: new[] { "ready" }
+                );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -71,6 +85,31 @@ namespace portfolio_backend
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+
+                endpoints.MapHealthChecks("/hc/ready", new HealthCheckOptions
+                {
+                    Predicate = (check) => check.Tags.Contains("ready"),
+                    ResponseWriter = async (context, report) =>
+                    {
+                        var result = JsonSerializer.Serialize(new
+                        {
+                            status = report.Status.ToString(),
+                            checks = report.Entries.Select(entry => new
+                            {
+                                name = entry.Key,
+                                status = entry.Value.Status.ToString(),
+                                exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                                duration = entry.Value.Duration.ToString()
+                            })
+                        });
+                        context.Response.ContentType = MediaTypeNames.Application.Json;
+                        await context.Response.WriteAsync(result);
+                    }
+                });
+                endpoints.MapHealthChecks("/hc/live", new HealthCheckOptions
+                {
+                    Predicate = (_) => false
+                });
             });
         }
     }
